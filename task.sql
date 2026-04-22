191cb96f5d4e9bf81ba5504f25ecbab49c92f2ab
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-04-22 21:13:55 +0300

    Normalize ShopDB to 3NF fixed

--- task.sql
+++ task.sql
@@ -3,23 +3,6 @@
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
-CREATE TABLE Countries (
-    ID INT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
-);
-
 -- Populate test data
 CREATE TABLE Countries (
 ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -51,11 +34,16 @@ CREATE TABLE ProductInventory (
 
 
 INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
+	VALUES ('Country1', 'Country2');
+
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Products (ID,Name)
+	VALUES (1, 'AwersomeProduct');
+    
+INSERT INTO Products (ID,Name)
+	VALUES (1, 'AwersomeProduct');
+
+INSERT INTO ProductInventory (ProductID, WarehouseID)
+	VALUES ( id, 'Warehouse-2');
+    
+
